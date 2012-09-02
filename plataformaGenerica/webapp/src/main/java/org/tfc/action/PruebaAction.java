554586432d6b21b0394aa5c498f1794cdae55a84
blancoparis <blancoparis@gmail.com>
2012-09-02 22:15:20 +0200

    Pagina resultado de la aplicación.

--- plataformaGenerica/webapp/src/main/java/org/tfc/action/PruebaAction.java
+++ plataformaGenerica/webapp/src/main/java/org/tfc/action/PruebaAction.java
@@ -1,12 +1,9 @@
 package org.tfc.action;
 
-import org.springframework.validation.AbstractBindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.webflow.execution.Event;
 import org.springframework.webflow.execution.RequestContext;
 import org.tfc.action.utils.ErroresDbpUtils;
 import org.tfc.form.PruebaForm;
-import org.tfc.form.subs.ErroresNegocioSubForm;
 
 public class PruebaAction extends BaseAction<PruebaForm>{
 
@@ -21,7 +18,9 @@ public class PruebaAction extends BaseAction<PruebaForm>{
 
 	public Event confirmar(RequestContext context) throws Exception {
 		PruebaForm form = (PruebaForm)getFormObject(context);
-		ErroresDbpUtils.procesarErrorNegocio(form, "Prueba de error");
+		if(form.getDescripcion().equals("error")){
+			ErroresDbpUtils.procesarErrorNegocio(form, "Prueba de error");
+		}
 		return resolucionEvent(context);
 	}
 	
---
+++ plataformaGenerica/webapp/src/main/webapp/WEB-INF/jsp/comunes/resultado.jsp
@@ -0,0 +1,13 @@
+<%@ include file="/WEB-INF/jsp/includes/taglibs.jsp" %>
+<fmt:requestEncoding value="UTF-8" />
+<fmt:setLocale value="${param.locale}"  />
+<fmt:bundle basename="app">
+	<article>
+		<section class="articuloDefecto">
+			<h1><font><font></font></font></h1>
+			<p>
+						
+			</p>
+		</section>
+	</article>
+</fmt:bundle>
